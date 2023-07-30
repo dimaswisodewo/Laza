75824223f3886099f81d8e654c268e7434c10016
dimaswisodewo <dimas@octagonstudio.com>
2023-07-30 21:18:08 +0700

    Add deep link to open FB, Twitter, and Gmail

--- Laza/Controllers/GetStartedViewController.swift
+++ Laza/Controllers/GetStartedViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class GetStartedViewController: UIViewController {
-
+    
     static let identifier = "GetStartedViewController"
     
     @IBOutlet weak var backButton: CircleButton! {
@@ -20,18 +20,21 @@ class GetStartedViewController: UIViewController {
     @IBOutlet weak var fbButton: RoundedButton! {
         didSet {
             fbButton.layer.cornerRadius = 10
+            fbButton.addTarget(self, action: #selector(fbButtonPressed), for: .touchUpInside)
         }
     }
     
     @IBOutlet weak var twitterButton: RoundedButton! {
         didSet {
             twitterButton.layer.cornerRadius = 10
+            twitterButton.addTarget(self, action: #selector(twitterButtonPressed), for: .touchUpInside)
         }
     }
     
     @IBOutlet weak var googleButton: RoundedButton! {
         didSet {
             googleButton.layer.cornerRadius = 10
+            googleButton.addTarget(self, action: #selector(googleButtonPressed), for: .touchUpInside)
         }
     }
     
@@ -51,7 +54,7 @@ class GetStartedViewController: UIViewController {
         super.viewDidLoad()
         
     }
-
+    
     @objc private func backButtonPressed() {
         navigationController?.popViewController(animated: true)
     }
@@ -64,5 +67,36 @@ class GetStartedViewController: UIViewController {
     @objc private func signInButtonPressed() {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: LoginViewController.identifier) else { return }
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func fbButtonPressed() {
+        openSharedURL(appURL: "fb://", webURL: "https://facebook.com/")
+    }
+    
+    @objc private func googleButtonPressed() {
+        openSharedURL(appURL: "googlegmail://", webURL: "https://mail.google.com/")
+    }
+    
+    @objc private func twitterButtonPressed() {
+        openSharedURL(appURL: "twitter://", webURL: "https://twitter.com/")
+    }
+    
+    private func openSharedURL(appURL: String, webURL: String) {
+        guard let appURL = URL(string: appURL) else {
+            print("Invalid URL: \(appURL)")
+            return
+        }
+        let application = UIApplication.shared
+    
+        if application.canOpenURL(appURL) {
+            application.open(appURL)
+        } else {
+            // if app is not installed, open URL inside Safari
+            guard let webURL = URL(string: webURL) else {
+                print("Invalid URL: \(webURL)")
+                return
+            }
+            application.open(webURL)
+        }
     }
 }
